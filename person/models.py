e44b93898b7f8d4008266132e2de4b8cbb156288
Matti Nelimarkka <matnel@humanisti.fixme.fi>
2011-09-19 14:03:29 +0300

    Refactored

--- person/models.py
+++ person/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Person(models.Model):
-    name = models.CharField()
-    image = models.CharField()
+    name = models.CharField( max_length = 200 )
+    image = models.CharField( max_length = 200 )
 
-class Connection(moderls.Model):
-    initial = models.ForeignKey( Person )
-    target = models.ForeignKey( Person )
+class Connection(models.Model):
+    fromPerson = models.ForeignKey( Person , related_name = 'from' )
+    toPerson = models.ForeignKey( Person ,  related_name = 'to' )
     weight = models.IntegerField()
