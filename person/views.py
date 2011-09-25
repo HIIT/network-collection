dad9f056bb041702d34692656ec72dc1a4844c3e
Matti Nelimarkka <matnel@humanisti.fixme.fi>
2011-09-26 00:59:10 +0300

    Add example content for persons

--- person/views.py
+++ person/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+from person.models import *
+
+def index( r ):
+    persons = Person.objects.all()
+    return render_to_response('person/index.html', {'users': persons } )
