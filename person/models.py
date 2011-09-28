8075af66cd94954b2029f80cca1820e8fe197b45
Matti Nelimarkka <matnel@humanisti.fixme.fi>
2011-09-28 23:17:45 +0300

    override string representations

--- person/models.py
+++ person/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField( max_length = 200 )
-    image = models.CharField( max_length = 200 )
+
+    def __str__(self):
+      return self.name
 
 class Connection(models.Model):
-    fromPerson = models.ForeignKey( Person , related_name = 'from' )
-    toPerson = models.ForeignKey( Person ,  related_name = 'to' )
+    fromPerson = models.ForeignKey( Person , related_name = 'from_set' )
+    toPerson = models.ForeignKey( Person ,  related_name = 'toPerson' )
     weight = models.IntegerField()
+    
+    def __str__(self):
+      return str( self.fromPerson.id ) + '->' + str( self.toPerson.id )
