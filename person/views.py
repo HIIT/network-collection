f99d60f5cd8e06293c629bebd5572e488160469c
Matti Nelimarkka <matnel@humanisti.fixme.fi>
2011-10-03 10:51:26 +0300

    JSON output format changed

--- person/views.py
+++ person/views.py
@@ -22,8 +22,9 @@ def data( r , id ):
 	    target = int( i[0][14:] )
             target = Person.objects.get( pk = target )
 	    weight = int( i[1] )
-            c = Connection( fromPerson = person, toPerson = target, weight = weight )
-            c.save()
+            if weight > 0:
+                c = Connection( fromPerson = person, toPerson = target, weight = weight )
+                c.save()
     return render_to_response('person/message.html', { 'message' : 'Data stored succesfully' } )
 
 def statistics(r):
@@ -32,7 +33,7 @@ def statistics(r):
     for p in persons:
          friends = []
          for c in p.from_set.all():
-             q = { 'nodeTo' : 'person_' + str( c.toPerson.id ) }
+             q = { 'nodeTo' : 'person_' + str( c.toPerson.id ) , 'data' : { '$lineWidth' : c.weight } }
              friends.append( q )
          p = { 'id' : 'person_' + str( p.id ) ,
                'name' : p.name ,
