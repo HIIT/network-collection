1efbfdf44701305c582a6a0bafba823d12e2a826
Matti Nelimarkka <matnel@humanisti.fixme.fi>
2011-10-10 12:02:03 +0300

    Add fied to indivate when questionare has been filled

--- person/models.py
+++ person/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField( max_length = 200 )
+    done = models.models.BooleanField(default=0)
 
     def __str__(self):
       return self.name
